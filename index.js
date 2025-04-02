a1c9a8fa830b03bf691c086f447dd10f77d5e256
Mellon Obada <mellobadah123@gmail.com>
2025-04-02 14:13:16 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const userData = { 
+        name: userName,
+        email: userEmail
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            const newId = object.id;
+            document.body.innerHTML += `<p>New ID: ${newId}</p>`;
+        })
+        .catch(function (error) {
+            document.body.innerHTML += `<p>${error.message}</p>`;
+        });
+}
